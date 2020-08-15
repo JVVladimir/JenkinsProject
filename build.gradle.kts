05400a0455b8008376f819682238703be1db7080
Vladimir <vakashitsyn@yandex.ru>
2020-08-15 21:40:38 +0300

    add test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 }
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+class Main {
+}
+
+fun main() {
+    println("Hello, from main code!")
+}
